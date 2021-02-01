612f9e113c747d16c6b95e502530fed72c0fbeed
ashwini-8 <ashwinip309@gmail.com>
2021-02-01 12:12:02 +0530

    Added: Created the objects and fetched value

--- OOPConcept.py
+++ OOPConcept.py
@@ -1,8 +1,19 @@
 print("Python using oops concept")
 
-class Dog:                             # creating dog class
+class Dog:                                           # creating dog class
+    species = "Animal"                               # class attributes
+
     def __init__(self, name, age, breed, color ):
-        self.name = name                          # instance attributes 
+        self.name = name                             # instance attributes
         self.age = age
         self.breed = breed
         self.color = color
+
+
+misty = Dog("misty", 9, "Havanese", "white")                                 # object creation
+rocky = Dog("rocky", 6, "German Shepherd", "brown")                          # object creation
+print(misty)
+print(rocky)
+print(misty.age)
+print(rocky.breed)
+print(misty == rocky )                                                # two different objects (False)
