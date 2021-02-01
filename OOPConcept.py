22bd9c923346827ddec3d53bd81d10d49bd71bfd
ashwini-8 <ashwinip309@gmail.com>
2021-02-01 11:55:02 +0530

    Added: Created __init__ method and instance attributes

--- OOPConcept.py
+++ OOPConcept.py
@@ -1,4 +1,8 @@
 print("Python using oops concept")
 
 class Dog:                             # creating dog class
-    pass                                
+    def __init__(self, name, age, breed, color ):
+        self.name = name                          # instance attributes 
+        self.age = age
+        self.breed = breed
+        self.color = color
